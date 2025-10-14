6e1e148d427f0a5e481138f5ca3c7bc2daa1f25a
chatchadaporn <chatchadaporn.noi@mnf2.rohmthai.com>
2025-10-14 16:36:15 +0700

    ver.25.0.0.0 - add get rack test and alter rack_id

---
+++ StoredProcedureDB/rcs/Stored Procedures/sp_get_rack_testControlVersion.sql
@@ -0,0 +1,25 @@
+﻿-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [rcs].[sp_get_rack_testControlVersion]
+	-- Add the parameters for the stored procedure here
+
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+	
+	SELECT 
+		rack_controls.id AS rack_id
+		, rack_controls.name AS rack 
+		, rack_categories.id AS category_id
+		, rack_categories.name AS category
+	FROM APCSProDB.rcs.rack_controls
+	INNER JOIN APCSProDB.rcs.rack_categories ON rack_controls.category = rack_categories.id
+
+END
